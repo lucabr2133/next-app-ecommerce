c8f2a89895756d272c6f42ddd44868037047976c
lucabr2133 <lucab2188@gmail.com>
2025-09-18 20:51:53 -0300

    Fix the fonts error

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import "@/styles/globals.css";
 import { Providers } from "./providers"
 import { NavbarComponet } from "@/UI/Components/Navbar"
-import { inter } from "@/UI/fonts";
+import { inter } from "@/fonts";
 export default function layout({children}:{children:React.JSX.Element}){
     return <> 
-        <html suppressHydrationWarning   lang="en" style={{colorScheme:'dark'}}>
+        <html className={inter.className} suppressHydrationWarning   lang="en" style={{colorScheme:'dark'}}>
         <head>
             <title>Document</title>
         </head>
-        <body className={`${inter.className} min-h-dvh dark text-foreground bg-background`}>
+        <body className={` min-h-dvh dark text-foreground bg-background`}>
             <Providers  >
                 <NavbarComponet></NavbarComponet>
                 {children}
