7032ccd30d6eb346d9123d880ac23e0c9400039a
lucabr2133 <lucab2188@gmail.com>
2025-11-10 16:32:10 -0300

    Fixing more deploy errors

--- app/(main)/checkout/success/page.tsx
+++ app/(main)/checkout/success/page.tsx
@@ -1,11 +1,11 @@
 import { cookies } from "next/headers"
 import { HomeSuccess } from "./(component)/homeSuccest"
 
-export default async function Page({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined } }){
-    const {orderId}= searchParams
+export default async function Page({ searchParams }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }){
+    const {orderId}= await searchParams
     const cookie=await cookies()
     const url=process.env.NEXT_PUBLIC_API_URL
-
+    
     const orderRequest= await fetch(`${url}/api/checkout/success?orderId=${orderId}`,{
         headers:{
             Cookie:cookie.toString()
